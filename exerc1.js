5c90436197feedc1bb53ceec5b6d917c1e525f62
LeandroMCSantos <102751955+LeandroMCSantos@users.noreply.github.com>
2022-05-05 12:54:09 -0300

    Add files via upload

---
+++ exerc1.js
@@ -0,0 +1,27 @@
+// let n = 100
+// for (i = 0; i < n; i++){
+//     console.log(i + 1)
+// }
+
+// let n = 100
+//  for (i = 0; i <= n; i++){
+//      if(i % 2 == 0)
+//      console.log(i);
+// }
+
+// let media = 0
+// let n = 5 
+// for (let i = 0; i < n; i++){
+//      let salario = Math.random() * 10000
+//      console.log("salario: ", salario)
+//      media += salario
+//  }
+//  media = media / n
+//  console.log("Média",media.toFixed(2))
+
+let salario = 15000
+let n = 5
+for (let i = 0; i < 5; i++){
+    salario = salario / 2
+    console.log('salario atualizado:',salario)
+}
